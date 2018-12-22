7cd94225d7ae7e44ef797f678aa31840ef488274
Hugo Corta Lorenzo <corta.hugo@gmail.com>
2018-12-22 17:45:50 +0100

    Add: General addings

---
+++ webpack.build.config.js
@@ -0,0 +1,81 @@
+const webpack = require('webpack')
+const path = require('path')
+const BabiliPlugin = require('babili-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
+const defaultInclude = path.resolve(__dirname, 'src')
+
+module.exports = {
+  // mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ],
+        include: defaultInclude
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: defaultInclude,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            emitWarning: true
+          }
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: defaultInclude,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/env',
+              '@babel/react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude
+      }
+    ]
+  },
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'bundle.css',
+      chunkFilename: '[id].css'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new BabiliPlugin()
+  ],
+  stats: {
+    colors: true,
+    children: false,
+    chunks: false,
+    modules: false
+  }
+}
