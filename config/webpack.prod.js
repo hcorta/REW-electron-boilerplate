fb1a50603005d9519bd74068de35c8755138f44d
Hugo Corta Lorenzo <corta.hugo@gmail.com>
2018-12-23 10:17:59 +0100

    General improvements

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -27,12 +27,12 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif)$/,
         use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude
+        include: [defaultInclude, path.resolve(__dirname, '../public')]
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude
+        include: [defaultInclude, path.resolve(__dirname, '../public')]
       }
     ]
   },
